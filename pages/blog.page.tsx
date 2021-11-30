3957c985155764bb1a8a43ff723b7fac3eb23de7
Jesse Bellingham <jbellingham91@gmail.com>
2021-11-30 21:55:41 +1100

    Added article element and styles to blog page

--- pages/blog.page.tsx
+++ pages/blog.page.tsx
@@ -5,6 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Date from "../components/date";
 import Layout from "../components/layout";
 import Head from "next/head";
+import styles from "./page.module.css";
 
 interface Props {
     allPosts: PostData[];
@@ -16,24 +17,26 @@ const BlogPage: NextPage<Props> = ({ allPosts }) => {
             <Head>
                 <title>All of my blog posts</title>
             </Head>
-            <section
-                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
-            >
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {allPosts.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
-            </section>
+            <article className={styles.page}>
+                <section
+                    className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+                >
+                    <h2 className={utilStyles.headingLg}>Blog</h2>
+                    <ul className={utilStyles.list}>
+                        {allPosts.map(({ id, date, title }) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                <Link href={`/posts/${id}`}>
+                                    <a>{title}</a>
+                                </Link>
+                                <br />
+                                <small className={utilStyles.lightText}>
+                                    <Date dateString={date} />
+                                </small>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </article>
         </Layout>
     );
 };
