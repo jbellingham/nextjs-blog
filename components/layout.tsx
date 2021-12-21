55a3034804d8a5dbb7345338e4a8759973ae9a16
Jesse Bellingham <jbellingham91@gmail.com>
2021-12-21 11:02:29 +1100

    Remove nextjs logo from links

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,12 +22,7 @@ export default function Layout({
                     name="description"
                     content="Personal website of Jesse Bellingham"
                 />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
+                <meta property="og:image" content="/images/profile.jpeg" />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
