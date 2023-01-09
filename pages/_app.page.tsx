8506a957a7cebfffa32134ef45cfbe55555bfd09
Jesse Bellingham <jbellingham91@gmail.com>
2023-01-10 09:51:45 +1100

    Add Vercel analytics

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -6,6 +7,11 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
 export default MyApp;
